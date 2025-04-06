cf3de5221ede528348711a0f765414e552ef34af
shimaa <shaimaafoda7@gmail.com>
2025-04-06 12:16:06 +0200

    response body .g generate done

--- lib/core/networking/api_result.dart
+++ lib/core/networking/api_result.dart
@@ -1,8 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:laa/core/networking/api_error_handler.dart';
-
+part 'api_result.freezed.dart';
 @Freezed()
 abstract class ApiResult<T> with _$ApiResult<T> {
   const factory ApiResult.success(T data) = Success<T>;
--- lib/features/auth/register/data/models/signup_request_body.dart
+++ lib/features/auth/register/data/models/signup_request_body.dart
@@ -1,5 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
-part 'sign_up_request_body.g.dart';
+part 'signup_request_body.g.dart';
 
 @JsonSerializable()
 class SignupRequestBody {
